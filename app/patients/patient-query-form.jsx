5d8b5c4517f1e9e798f344eacaca2f5867edef62
Tony L. Kerz <tony@WA716948A-HFD-OEM.local>
2016-01-31 09:13:13 -0500

    [app:patient:search] react-select

--- app/patients/patient-query-form.jsx
+++ app/patients/patient-query-form.jsx
@@ -1,17 +1,18 @@
 import debug from 'debug'
 import React, {Component} from 'react'
 import {reduxForm} from 'redux-form'
-// import Select from 'react-select'
-// import Select2 from '../shared/select2'
+//import Select from 'react-select'
+import Select from '../shared/select'
+//import Select2 from '../shared/select2'
 
 const dbg = debug('app:patients:query')
 
-// const sortOptions = [
-//   {value: '+firstName', label: 'first name (descending)'},
-//   {value: '-firstName', label: 'first name (ascending)'},
-//   {value: '+lastName', label: 'last name (descending)'},
-//   {value: '-lastName', label: 'last name (ascending)'}
-// ]
+const sortOptions = [
+  {value: '+firstName', label: 'first name (descending)'},
+  {value: '-firstName', label: 'first name (ascending)'},
+  {value: '+lastName', label: 'last name (descending)'},
+  {value: '-lastName', label: 'last name (ascending)'}
+]
 
 function validate(values, props) {
   dbg('validate: values=%o, props=%o', values, props)
@@ -57,8 +58,9 @@ export default class PatientQueryForm extends Component {
         </div>
         <div className='form-group'>
           <label htmlFor='sort'>sort: </label>
-          <input type='text' className='form-control' id='sort' placeholder='sort' {...sort}/>
           {/*
+          <input type='text' className='form-control' id='sort' placeholder='sort' {...sort}/>
+          */}
           <Select
             name='sort'
             value='+lastName'
@@ -66,6 +68,7 @@ export default class PatientQueryForm extends Component {
             onBlurResetsInput={false}
             {...sort}
           />
+          {/*
           <Select2
             name='sort2'
             value='+lastName'
